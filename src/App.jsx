a5f6a47a22fed5f483194e26a97c0f210f531604
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-06-04 14:00:21 +0545

    Gallery through lighgallery package

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Gallery } from './Gallery'
+import './style.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Gallery/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Gallery.jsx
@@ -0,0 +1,61 @@
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+const images =[
+    {src:"/7.jpg", alt:"7"},
+    {src:"/2.jpg", alt:"2"},
+    {src:"/3.jpg", alt:"3"},
+    {src:"/4.jpg", alt:"4"},
+    {src:"/5.jpg", alt:"5"},
+    {src:"/6.jpg", alt:"6"},
+    {src:"/1.png", alt:"Nepal"},
+    {src:"/8.jpg", alt:"8"},
+    {src:"/9.jpg", alt:"9"},
+    {src:"/10.jpg", alt:"10"},
+    {src:"/11.jpg", alt:"11"},
+    {src:"/12.jpg", alt:"12"},
+    {src:"/13.jpg", alt:"13"},
+    {src:"/14.jpg", alt:"14"},
+    {src:"/15.jpg", alt:"15"},
+    {src:"/16.jpg", alt:"16"},
+    {src:"/17.jpg", alt:"17"},
+    {src:"/18.jpg", alt:"18"},
+    {src:"/19.jpg", alt:"19"}
+]
+
+
+export function Gallery() {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+    return (
+        <div className="App">
+            <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom]}
+            >
+
+                {images.map((image,index)=>{
+                    return(
+                        <a href={image.src} key={index} >
+                        <img alt={image.alt} src={image.src} />
+                    </a>
+                    )
+                })}
+
+           
+            </LightGallery>
+        </div>
+    );
+}
