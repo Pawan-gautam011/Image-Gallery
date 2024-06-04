27af484da99bbafd2e067e99587b14ef5e240980
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-06-04 14:04:59 +0545

    Extra features added

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,12 +4,22 @@ import LightGallery from 'lightgallery/react';
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-autoplay.css';
+import 'lightgallery/css/lg-fullscreen.css';
+import 'lightgallery/css/lg-share.css';
+import 'lightgallery/css/lg-rotate.css';
 
 
 
 // import plugins if you need
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
+import lgAutoplay from 'lightgallery/plugins/autoplay';
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import lgShare from 'lightgallery/plugins/share';
+import lgRotate from 'lightgallery/plugins/rotate';
+
+
 
 const images =[
     {src:"/7.jpg", alt:"7"},
@@ -43,7 +53,7 @@ export function Gallery() {
             <LightGallery
                 onInit={onInit}
                 speed={500}
-                plugins={[lgThumbnail, lgZoom]}
+                plugins={[lgThumbnail, lgZoom,lgAutoplay,lgFullscreen,lgRotate,lgShare]}
             >
 
                 {images.map((image,index)=>{
